da3056fe2bb02cb54de86f78a5391f8d52b65b65
Grenui92 <grenui92@gmail.com>
2023-02-25 23:17:40 +0200

    async and insert()

--- db_scr/json_files.py
+++ db_scr/json_files.py
@@ -1,37 +1,49 @@
 from models import Author, Quote
 import json
+import aiofiles
 import connect
 
 
-def load_authors_from_file():
-    with open('json_files/authors.json', 'r') as file:
-        data = json.load(file)
+async def load_authors_from_file():
+    async with aiofiles.open('json_files/authors.json', mode='r') as file:
+        res = await file.read()
+        data = json.loads(res)
+
+    author_list = []
 
     for record in data:
         author = Author(fullname=record['fullname'],
                         born_date=record['born_date'],
                         born_location=record['born_location'],
                         description=record['description'])
-        author.save()
+        author_list.append(author)
 
-def load_quotes_from_file():
-    with open('json_files/quotes.json', 'r') as file:
-        data = json.load(file)
+    Author.objects.insert(author_list)
 
+async def load_quotes_from_file():
+    async with aiofiles.open('json_files/quotes.json', 'r') as file:
+        res = await file.read()
+        data = json.loads(res)
+
+    quote_list = []
     for record in data:
 
         if record['author'] == 'Alexandre Dumas fils':
-            print(record['author']) #Я про цей випадок пита в слак
+            #Я про цей випадок пита в слак
             record['author'] = 'Alexandre Dumas-fils'
+
         author = Author.objects(fullname=record['author']).first()
 
         try:
             quote = Quote(tags=record['tags'],
                            author=author.id,
                            quote=record['quote'])
-            quote.save()
+            quote_list.append(quote)
+
         except AttributeError:
             print(f'I cant find author "{record["author"]}"')
+
+    Quote.objects.insert(quote_list)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import asyncio
+
 from scrapy.crawler import CrawlerProcess
 
 from homework.homework.spiders.quotes import QuotesSpider
 from homework.homework.spiders.authors import AuthorsSpider
 from db_scr.json_files import load_quotes_from_file, load_authors_from_file
 
-if __name__ == '__main__':
+async def main():
     proces = CrawlerProcess()
 
     proces.crawl(QuotesSpider)
@@ -12,5 +14,8 @@ if __name__ == '__main__':
 
     proces.start()
 
-    load_authors_from_file()
-    load_quotes_from_file()
+    await load_authors_from_file()
+    await load_quotes_from_file()
+
+if __name__ == '__main__':
+    asyncio.run(main())
